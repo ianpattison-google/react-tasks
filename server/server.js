3b010d2036651168d8cab68336670b5419aae22a
Ian Pattison <ianpattison@google.com>
2020-09-21 22:38:22 +0100

    define PORT

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('react-tasks', 'root', 'password', {
